4ab335b5df5c5eabc2aceae1ea4aa44accb4ef45
Ayraazeema <149694291+Ayraazeema@users.noreply.github.com>
2023-11-10 12:18:00 +0530

    Add files via upload

---
+++ calcy app.py
@@ -0,0 +1,32 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+while True:
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'quit' to end the program")
+
+    user_input = input(": ")
+
+    if user_input == "quit":
+        break
+    elif user_input in ("add", "subtract", "multiply"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if user_input == "add":
+            print("Result: " + str(add(num1, num2)))
+        elif user_input == "subtract":
+            print("Result: " + str(subtract(num1, num2)))
+        elif user_input == "multiply":
+            print("Result: " + str(multiply(num1, num2)))
+    else:
+        print("Invalid input")
